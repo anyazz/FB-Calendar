5505bc74a80e9fbcda3f722bfec6b147920e7cc7
Anya Zhang <Anya@Anyas-MacBook.local>
2017-01-18 23:32:33 -0700

    first commit - V1.1

---
+++ js/extension.js
@@ -0,0 +1,191 @@
+// initialize variables
+var event = "";
+var tries = 0;
+var loadError = false;
+
+window.onload = extension;
+
+/*
+ If error arises while running extension, wait and try again before changing loadError to true
+*/
+function error() {
+	if (tries == 8) {
+		loadError = true;
+	} else {
+		tries += 1;
+		console.log("trying extension again :", tries);
+		setTimeout(extension, 250);
+	}
+}
+
+/*
+ Main extension function - retrieves info from DOM and adds to event object
+*/
+function extension() {
+	$(document).ready(function () {
+		try {
+			// GET EVENT TITLE
+			var title = "";
+			var temp = $("[data-testid='event-permalink-event-name']")[0];
+			if (typeof temp !== 'undefined') {
+				title = temp.innerText
+			} else {
+				return error();
+			}
+
+			// set of functions to get text from within HTML tag, separated by spaces or line breaks  
+			// adapted from http://stackoverflow.com/questions/2836317/using-jquery-to-gather-all-text-nodes-from-a-wrapped-set-separated-by-spaces
+			function collectTextNodes(element, texts) {
+				for (var child = element.firstChild; child !== null; child = child.nextSibling) {
+					if (child.nodeType === 3)
+						texts.push(child);
+					else if (child.nodeType === 1)
+						collectTextNodes(child, texts);
+				}
+			}
+
+			function getTextWithSpaces(element) {
+				var texts = [];
+				collectTextNodes(element, texts);
+				for (var i = texts.length; i-- > 0;)
+					texts[i] = texts[i].data;
+				return texts.join(' ');
+			}
+
+			function getTextWithBreaks(element) {
+				var texts = [];
+				collectTextNodes(element, texts);
+				for (var i = texts.length; i-- > 0;)
+					texts[i] = texts[i].data;
+				return texts.join('\n');
+			}
+
+			// get text from Facebook's event summary
+			var element = document.getElementById("event_summary")
+			var textWithSpaces = getTextWithSpaces(element);
+			var textWithBreaks = getTextWithBreaks(element);
+
+			// GET ADDRESS
+			var address = "";
+			
+			// temp1: if address is linked to map; temp2: address is plain text
+			var temp1 = textWithSpaces.match("Hide Map (.*) Report")
+			var temp2 = textWithBreaks.match("pin\n(.*)")
+			if (temp1 !== null) {
+				address = temp1[1];
+			} else if (temp2 !== null) {
+				address = temp2[1]
+			}
+
+			// GET START AND END TIMES
+			// uses MIT's indispensable Moment.js (momentjs.com) 
+
+			// get Facebook-defined startDate for event 
+			var startDT = $("[itemprop='startDate']").attr('content');
+			var endDT = ""
+
+			// use RegEx to define temporary start and end
+			var start = textWithBreaks.match("at \n(.*)\n");
+			var end = textWithBreaks.match("- \n(.*)\n");
+			if (end === null) {
+				// replace normal dash w/ em-dash
+				end = textWithBreaks.match("– \n(.*)\n");
+			}
+
+			// if start exists, first line is of form "Date at time (- time)"
+			if (start !== null) {
+				start = moment(start[1], 'h A')
+
+				// if end exists, event has end time
+				if (end !== null) {
+					end = moment(end[1], 'h A')
+
+					// calculate difference bet. start and end
+					var diff = moment.duration(end.diff(start));
+
+					// if event goes overnight, diff may be negative (ex. 10 PM to 2 AM) - subtract from 24 hrs
+					if (diff._milliseconds < 0) {
+						diff._milliseconds = 86400000 + diff._milliseconds;
+					}
+
+					// add difference to startDT for endDT
+					var sum = moment(startDT, moment.ISO_8601).add(diff)
+					endDT = sum.format()
+				}
+
+				// else, automatically make event one hour long
+				else {
+					var sum = moment(startDT, moment.ISO_8601).add(1, 'hours')
+					endDT = sum.format()
+				}
+			}
+			// else if start doesn't exist (first line of form "Date - Date")
+			else {
+				// time information then located in second line; similar process
+				var line2 = textWithBreaks.match(end[1] + "\n(.*)\n")
+				start = line2[1].match("(.*) to")[1]
+				end = line2[1].match("to (.*)M")[1]
+				end = moment(end, 'MMM D at h:MM A')
+				start = moment(start, 'MMM D at h:MM A')
+				var diff = moment.duration(end.diff(start));
+				var sum = moment(startDT, moment.ISO_8601).add(diff)
+				endDT = sum.format()
+			}
+
+			// GET DESCRIPTION
+			// use id if available; else by classes
+			var description = ""
+			if ($("#event_description").length > 0) {
+				description = $("#event_description").text();
+			} else {
+				description = $("._4n-j._3cht.fsl").text();
+			}
+
+			// add info to event object	
+			event = {
+				'summary': title,
+				'location': address,
+				'description': description,
+				'start': {
+					'dateTime': startDT,
+				},
+				'end': {
+					'dateTime': endDT,
+				},
+			};
+		}
+
+		// run error function if any errors thrown
+		catch (err) {
+			console.log(err.message);
+			error();
+		}
+	})
+};
+
+// listen for messages
+chrome.runtime.onMessage.addListener(
+	function (message, sender, sendResponse) {
+		switch (message.type) {
+			// send event when popup asks
+			case "getEvent":
+				sendResponse(event);
+				break;
+
+				// if URL changed, re-run extension
+			case "tabUpdated":
+				sendResponse("received");
+				extension();
+				break;
+
+				// tell popup if loading error occurred 
+			case "checkError":
+				sendResponse(loadError);
+				break;
+
+				// else, log error
+			default:
+				console.error("Unrecognised message: ", message);
+		}
+	}
+);
---
+++ js/popup.js
@@ -0,0 +1,230 @@
+var CLIENT_ID = '959780096527-nk761kkom2cv7mvnodi9b2l018o43p9n.apps.googleusercontent.com';
+var SCOPES = ["https://www.googleapis.com/auth/calendar"];
+var eventSuccess = false;
+
+/*
+ On load, check for Google authentication
+*/
+window.onload = function () {
+	checkLoad();
+	checkAuth();
+};
+
+/*
+Check extension.js load status
+*/
+function checkLoad() {
+	chrome.tabs.query({
+		active: true,
+		currentWindow: true
+	}, function (tabs) {
+
+		// send message to extension.js
+		chrome.tabs.sendMessage(tabs[0].id, {
+			type: "checkError"
+		}, function (loadError) {
+
+			// if loadError, report; else, keep checking until event successfully obtained
+			if (loadError) {
+				report("load-error")
+			} else if (!eventSuccess) {
+				setTimeout(checkLoad, 250)
+			}
+		})
+	})
+
+}
+/*
+ Initiate email.js functionality (emailjs.com)
+*/
+(function () {
+	emailjs.init("user_Eng6TCgy2E3S4Q1Tz1Gcq");
+})();
+
+
+/**
+ * Check if current user has authorized this application.
+ * Authorization process adapted from https://developers.google.com/google-apps/calendar/quickstart/js
+ */
+function checkAuth() {
+	gapi.auth.authorize({
+		'client_id': CLIENT_ID,
+		'scope': SCOPES.join(' '),
+		'immediate': true
+	}, handleAuthResult);
+}
+
+/**
+ * Handle response from authorization server.
+ *
+ * @param {Object} authResult Authorization result.
+ */
+function handleAuthResult(authResult) {
+	if (authResult && !authResult.error) {
+		// Hide auth UI, then load client library.
+		show('authorize-div', false)
+		loadCalendarApi();
+	} else {
+		// Show auth UI, allowing the user to initiate authorization by
+		// clicking authorize button.
+		show('authorize-div', true);
+	}
+}
+
+/**
+ * Load Google Calendar client library.
+ */
+function loadCalendarApi() {
+	gapi.client.load('calendar', 'v3', getEvent);
+}
+
+/*
+If automatic check fails, allow user to initiate auth flow by clicking button.
+*/
+document.getElementById("authorize-button").addEventListener('click', function () {
+	handleAuthClick(event);
+});
+
+/**
+ * Iinitiate auth flow in response to user clicking authorize button.
+ *
+ * @param {Event} event Button click event.
+ */
+function handleAuthClick(event) {
+	gapi.auth.authorize({
+			client_id: CLIENT_ID,
+			scope: SCOPES,
+			immediate: false
+		},
+		handleAuthResult);
+	return false;
+}
+
+/**
+ * Send message to extension.js to get event information from DOM
+ */
+var tries = 0;
+
+function getEvent() {
+	chrome.tabs.query({
+		active: true,
+		currentWindow: true
+	}, function (tabs) {
+		chrome.tabs.sendMessage(tabs[0].id, {
+			type: "getEvent"
+		}, function (event) {
+			try {
+				updateInfo(event);
+			} catch (err) {
+				// keep trying until successful or max tries reached
+				if (tries > 15) {
+					report("display-error")
+					console.log(err.message);
+				} else {
+					tries += 1
+					console.log("trying get again :", tries)
+					setTimeout(getEvent, 250);
+				}
+			}
+		})
+	});
+}
+
+/*
+ Update popup window event information
+*/
+function updateInfo(resource) {
+	// format time using Moment.js
+	var time = moment(resource.start.dateTime, moment.ISO_8601).format("MMMM Do YYYY, h:mm a");
+
+	// cap description at 95 characters, rounded to nearest word
+	var description = ""
+	var word = ""
+	for (var i = 0; i < 95; i++) {
+		if (resource.description[i] === " " || i === resource.description.length) {
+			description += word;
+			word = " ";
+		} else {
+			word += resource.description[i];
+		}
+	}
+	// add ellipses if description truncated
+	if (resource.description.length > 95) {
+		description += "..."
+	}
+	
+	// update fields
+	document.getElementById('title').innerHTML = resource.summary;
+	document.getElementById('start-time').innerHTML = time;
+	document.getElementById('location').innerHTML = resource.location;
+	document.getElementById('description').innerHTML = description;
+	
+	// update values and popup appearance
+	eventSuccess = true;
+	show("loading-div", false)
+	show("event-info", true);
+	show("buttons", true);
+
+	// Upon button click, execute addEvent function
+	document.getElementById("buttons").addEventListener('click', function () {
+		gapi.client.load('calendar', 'v3', addEvent(resource));
+	})
+}
+
+/* 
+* Insert event into calendar
+* adapted from https://developers.google.com/google-apps/calendar/v3/reference/events/insert 
+ */
+function addEvent(resource) {
+	var request = gapi.client.calendar.events.insert({
+		'calendarId': 'primary',
+		'resource': resource
+	});
+
+	// upon completion, replace add button with success
+	request.execute(function (resp) {
+		document.getElementById("add-btn").src = "icons/success.png";
+		document.getElementById("add-btn-hover").src = "icons/success.png"
+		console.log(resp)
+	})
+}
+
+/*
+* Shortcut function for hiding/displaying divs
+*/
+function show(id, value) {
+	document.getElementById(id).style.display = value ? 'block' : 'none';
+}
+
+/*
+* Report bugs
+*/
+function report(bug_id) {
+	// show error divs
+	show("loading-div", false);
+	show(bug_id, true);
+	var button_id = bug_id.match("(.*)error")[1] + "report"
+	
+	// get tab information
+	chrome.tabs.query({
+		active: true,
+		currentWindow: true
+	}, function (tabs) {
+		
+		// upon button click, send automated bug report with necessary info using emailjs
+		document.getElementById(button_id).addEventListener('click', function () {
+			var parameters = {
+				"url": tabs[0].url,
+				"time": moment().format("dddd, MMMM Do YYYY, h:mm:ss a"),
+				"location": moment.tz.guess(),
+				"identity": bug_id
+			}
+			emailjs.send("gmail", "fbcalendar_bug", parameters)
+				.then(function (response) {
+					console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
+				}, function (err) {
+					console.log("FAILED. error=", err);
+				});
+		})
+	})
+}
